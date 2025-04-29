0d58d12a356eb0706c855a4d0ea32164d4c628ab
Saymon Farias Alberto <119527512+SaymonFarias@users.noreply.github.com>
2025-04-29 16:20:14 -0300

    navbar
    Criação de uma nova navbar lateral, que fara a navegação entre paginas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,32 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormComponent } from './form/form.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redireciona para login por padrão
-  { path: 'login', component: LoginComponent }, // Página de login
-  { path: 'form', component: FormComponent } // Página de formulário
+  { path: 'login', component: LoginComponent }, // Login sem a barra lateral
+  {
+    path: '',
+    component: SidebarComponent, // Barra lateral encapsula as demais rotas
+    children: [
+      { path: 'form', component: FormComponent } // Página de formulário
+    ]
+  },
+  { path: '**', redirectTo: '/login' } // Redireciona para login se a rota não for encontrada
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FormComponent
+    FormComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes) // Configuração das rotas
+    RouterModule.forRoot(routes)
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
